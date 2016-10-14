22dd8b5fddc518acde1b3202f0174c7e4e8a2101
Aleks Gorbenko <kucher12@gmail.com>
2016-10-14 18:45:10 +0100

    boilerplate controller, view engines

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
+var todoController = require('./controllers/todoController');
 
-var http = require('http');
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+
+todoController(app);
 
 app.listen(3000);
 console.log('listening to port 3000');
